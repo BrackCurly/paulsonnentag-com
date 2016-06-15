6f69a4bda7116e0b040c38751f558847c147da3f
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2016-06-15 23:54:58 +0200

    Fix event preventDefault

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
 (function () {
   'use strict';
 
-  addEventListener(document, 'click', 'a[href*="#"]:not([href="#"])', function (evt) {
+  var selectedProject;
+
+  addEventListener(document, 'click', 'a[data-scroll-smooth]', function (evt) {
     var scrollY;
     var id = evt.target.href.split('#')[1];
-    var anchor = document.querySelector('a[name="' + id + '"]');
+    var anchor = document.getElementById(id);
 
     if (anchor) {
       scrollY = Math.min(anchor.offsetTop, document.body.scrollHeight - document.body.clientHeight);
@@ -14,70 +16,68 @@
     }
   });
 
-  addEventListener(document, 'click', 'a[data-open-modal]', function (evt) {
-    var el = evt.target;
-    var modalContainer = document.getElementById('modal-container');
 
-    evt.preventDefault();
-
-
-    console.log('scroll');
-
-    el.classList.add('loading');
-
-    request(el.href, function (evt) {
-      var parser = new DOMParser();
-      var dom = parser.parseFromString(evt.currentTarget.response, "text/html");
-      var offset = el.getBoundingClientRect();
-
-      modalContainer.innerHTML = '';
-
-      var modal = document.createElement('div');
-      modal.innerHTML = dom.body.innerHTML;
-      modal.classList.add('modal');
-      modal.style.width = el.clientWidth + 'px';
-      modal.style.height = el.clientHeight + 'px';
-      modal.style.transform = 'translate3d(' + offset.left + 'px, ' + offset.top + 'px, 0)';
-      modal.style.opacity = 0;
-
-      modalContainer.style.transform = 'translate3d(0, ' + window.scrollY + 'px, 0)';
-      modalContainer.classList.remove('hidden');
-
-      setTimeout(function () {
-        modal.style.width = '100%';
-        modal.style.height = '100%';
-        modal.style.transform = 'translate3d(0, 0, 0)';
-        modal.style.opacity = 1;
-      }, 50);
-
-      modalContainer.appendChild(modal);
-
-      el.classList.remove('loading');
-    });
-  });
-
-
-
-  document.querySelectorAll('a[data-open-modal]')
+  Array.prototype.slice.call(document.querySelectorAll('a[data-open-modal]'))
     .forEach(function (el) {
 
-      console.log(el);
-
       el.addEventListener('click', function (evt) {
-        console.log('disrupt click');
+        var modalContainer = document.getElementById('modal-container');
+
+        selectedProject = el;
 
         evt.preventDefault();
+
+        el.classList.add('loading');
+
+        request(el.href, function (evt) {
+          var parser = new DOMParser();
+          var dom = parser.parseFromString(evt.currentTarget.response, "text/html");
+          var offset = el.getBoundingClientRect();
+
+          modalContainer.innerHTML = '';
+
+          var modal = document.createElement('div');
+          modal.id = 'modal';
+          modal.innerHTML = dom.body.innerHTML;
+          modal.classList.add('modal');
+          modal.style.width = el.clientWidth + 'px';
+          modal.style.height = el.clientHeight + 'px';
+          modal.style.transform = 'translate3d(' + offset.left + 'px, ' + offset.top + 'px, 0)';
+          modal.style.opacity = 0;
+
+          modalContainer.classList.remove('hidden');
+
+          setTimeout(function () {
+            modal.style.width = '100%';
+            modal.style.height = '100%';
+            modal.style.transform = 'translate3d(0, 0, 0)';
+            modal.style.opacity = 1;
+          }, 50);
+
+          modalContainer.appendChild(modal);
+
+          el.classList.remove('loading');
+        });
       });
     });
 
   addEventListener(document, 'click', 'a[data-modal-close]', function (evt) {
     var modalContainer = document.getElementById('modal-container');
-    modalContainer.classList.add('hidden');
 
-    console.log(modalContainer);
+
+    var offset = selectedProject.getBoundingClientRect();
+
+    var modal = document.getElementById('modal');
+    modal.style.width = selectedProject.clientWidth + 'px';
+    modal.style.height = selectedProject.clientHeight + 'px';
+    modal.style.transform = 'translate3d(' + offset.left + 'px, ' + offset.top + 'px, 0)';
+    modal.style.opacity = 0;
+
+    modal.addEventListener('transitionend', function () {
+      modalContainer.classList.add('hidden');
+    });
 
     evt.preventDefault();
-    evt.stopPropagation();
   });
 
   function addEventListener (element, eventType, selector, callback) {
